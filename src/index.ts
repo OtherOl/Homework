28a07d4c58edf9b8efd1242f231e609e0b2df03d
OtherOl <pilyak003@gmail.com>
2023-09-15 21:38:45 +0300

    1-st homework

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,80 @@ import bodyParser from "body-parser";
 
 const app = express()
 const port = 4000
-
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
-    let helloWorld = "Hello, let's start"
-    res.send(helloWorld)
+let videos = [{
+    id: +(new Date()),
+    title: "Homework",
+    author: "Pilya",
+    canBeDownloaded: true,
+    createdAt: new Date(),
+    publicationDate: new Date(),
+    availableResolutions: ["P144"]
+}]
+
+app.get('/videos', (req: Request, res: Response) => {
+    res.status(200).send(videos)
+})
+
+app.post('/videos', (req: Request, res: Response) => {
+    const quality = req.body.title
+    if(!quality || typeof quality !== "string" || quality.length > 40 || !quality.trim()) {
+        res.status(400).send({
+            errorsMessages: [{
+                message: 'Incorrect title',
+                field: 'title'
+            }]
+        })
+        return;
+    }
+    const newVideo = {
+        id: +(new Date()),
+        title: req.body.title,
+        author: 'Pilya',
+        canBeDownloaded: true,
+        createdAt: new Date(),
+        publicationDate: new Date(),
+        availableResolutions: ["P144"]
+    }
+    videos.push(newVideo)
+    res.status(201).send(newVideo)
+})
+
+app.get('/videos/:id', (req: Request, res: Response) => {
+    let video = videos.find(p => p.id === +(req.params.id))
+    if(video) {
+        res.status(200)
+    } else {
+        res.status(404)
+    }
+})
+
+app.put('/videos/:id', (req: Request, res: Response) => {
+    let update = videos.find(p => p.id === +(req.params.id))
+    if(update) {
+        update.title = req.body.title
+        res.status(204)
+    } else if(!req.body.title || typeof req.body.title !== "string" || req.body.title.length > 40 || !req.body.title.trim()) {
+        res.status(404).send({
+            errorsMessages: [{
+                message: 'Incorrect title',
+                field: 'title'
+            }]
+        })} else {
+        res.status(400)
+    }
+})
+
+app.delete('/videos/:id', (req: Request, res: Response) => {
+    for(let i = 0; i < videos.length; i++) {
+        if(+(req.params.id) === videos[i].id) {
+            videos.splice(i, 1)
+            res.status(204)
+        } else {
+            res.status(404)
+        }
+    }
 })
 
 const parserMiddleware = bodyParser({})
