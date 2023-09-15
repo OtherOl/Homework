2b7ead7d37512a0e56c41ea35348141bb08f2cff
OtherOl <pilyak003@gmail.com>
2023-09-16 00:42:22 +0300

    1-st homework

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,101 @@
 import express, {Request, Response} from "express";
 import bodyParser from "body-parser";
-import {videosRouter} from "./routes/videos-router";
 
+let videos = [{
+    id: +(new Date()),
+    title: "Homework",
+    author: "Pilya",
+    canBeDownloaded: true,
+    createdAt: new Date().toISOString(),
+    publicationDate: new Date().toISOString(),
+    availableResolutions:  ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160']
+}]
 
 const app = express()
 const port = 4000
 app.use(express.json())
 
+app.get('/videos', (req: Request, res: Response) => {
+    res.status(200)
+})
+
+app.post('/videos', (req: Request, res: Response) => {
+    const quality = req.body.title
+    const name = req.body.author
+    if(!quality || typeof quality !== "string" || quality.length > 40 || !quality.trim()) {
+        res.status(400).send({
+            errorsMessages: [{
+                message: 'Incorrect title',
+                field: 'title'
+            }]
+        })
+    } else if(!name || typeof name !== "string" || name.length > 20 || !name.trim()) {
+        res.status(400).send({
+            errorsMessages: [{
+                message: 'Incorrect author',
+                field: 'author'
+            }]
+        })
+        return;
+    }
+    const newVideo = {
+        id: +(new Date()),
+        title: req.body.title,
+        author: 'Pilya',
+        canBeDownloaded: true,
+        createdAt: new Date().toISOString(),
+        publicationDate: new Date().toISOString(),
+        availableResolutions: ["P144"]
+    }
+    videos.push(newVideo)
+    res.status(201).send(newVideo)
+})
+
+app.get('/videos/:id', (req: Request, res: Response) => {
+    let video = videos.find(p => p.id === +(req.params.id))
+    if(video) {
+        res.status(200)
+    } else {
+        res.status(404)
+    }
+})
+
+app.put('/videos/:id', (req: Request, res: Response) => {
+    let update = videos.find(p => p.id === +(req.params.id))
+    if(update) {
+        if(!req.body.title || typeof req.body.title !== "string" || req.body.title.length > 40 || !req.body.title.trim()) {
+            res.status(400).send({
+                errorsMessages: [{
+                    message: 'Incorrect title',
+                    field: 'title'
+                }]
+            })} else if(!update){
+            res.status(404)
+        } else {
+            update.title = req.body.title
+            res.status(204)
+        }
+    }
+})
+
+app.delete('/videos/:id', (req: Request, res: Response) => {
+    for(let i = 0; i < videos.length; i++) {
+        if(+(req.params.id) === videos[i].id) {
+            videos.splice(i, 1)
+            res.status(204)
+        } else {
+            res.status(404)
+        }
+    }
+})
+
+app.delete('/videos/testing/all-data', (req: Request, res: Response) => {
+    videos.splice(0)
+    res.status(204)
+})
+
 const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
-
-app.use('/videos', videosRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
