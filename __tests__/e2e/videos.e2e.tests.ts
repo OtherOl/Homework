00f4a9c6c7dc36036a5cd23fc95a7ad9c2fd58b7
OtherOl <pilyak003@gmail.com>
2023-09-18 23:52:29 +0300

    modified

--- __tests__/e2e/videos.e2e.tests.ts
+++ __tests__/e2e/videos.e2e.tests.ts
@@ -1,12 +1,39 @@
 import request from 'supertest'
 import {app} from "../../src/setting";
 import {videos} from "../../src/routes/video-router";
+import {erAuthor, erTitle} from "../../src/errors/errors-fields";
 
 
 describe('/videos', () => {
-    it('Get video - success - ]', async () => {
+    it('GET video - success - ]', async () => {
         await request(app)
             .get('/videos')
             .expect(videos)
+    })
+
+    it('POST video - fail - invalid', async () => {
+        await request(app)
+            .post('/videos')
+            .send({title: "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd", author: ''})
+            .expect(400, {
+                errorsMessages: [
+                    erTitle,
+                    erAuthor
+                ]
+            })
+    })
+
+    it('POST video - success', async () => {
+        await request(app)
+            .post('/videos')
+            .send({
+                id: Number(new Date()),
+                title: 'Pilya',
+                author: 'Dima',
+                canBeDownloaded: false,
+                minAgeRestriction: 16,
+                availableResolutions: ['P1080'],
+            })
+            .expect(201)
     })
 })
---
+++ src/errors/errors-fields.ts
@@ -0,0 +1,20 @@
+export const erAuthor = {
+    message: 'Incorrect author',
+    field: 'author'
+}
+export const erTitle = {
+    message: 'Incorrect title',
+    field: 'title'
+}
+export const erAvailableResolutions = {
+    message: 'Incorrect availableResolutions',
+    field: 'availableResolutions'
+}
+export const erCanBeDownloaded = {
+    message: 'Incorrect canBeDownloaded',
+    field: 'canBeDownloaded'
+}
+export const erMinAgeRestriction = {
+    message: 'Incorrect minAgeRestriction',
+    field: 'minAgeRestriction'
+}
