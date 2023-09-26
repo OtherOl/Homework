24e5f51cba91871f2762cff92e2238a773a37994
OtherOl <pilyak003@gmail.com>
2023-09-26 20:27:22 +0300

    modified

--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -8,7 +8,7 @@ postsRouter.get('/', (req: Request, res: Response) => {
 })
 
 postsRouter.post('/', (req: Request, res: Response) => {
-    const {title , shortDescription, content, blogId} = req.params
+    const {title , shortDescription, content, blogId} = req.body
 
     const newPost = postsRepository.createPost(req.body)
     res.status(201).send(newPost)
