fef65e4d2f4474a16f6a4409752482cf4963d060
OtherOl <pilyak003@gmail.com>
2023-10-09 21:06:44 +0300

    Find Posts by blogId

--- src/repositories/posts-db-repository.ts
+++ src/repositories/posts-db-repository.ts
@@ -12,7 +12,7 @@ export const postsRepository = {
         const foundPost: postModel[] = await clientPostCollection
             .find({}, {projection: {_id: 0}})
             .sort(sortQuery)
-            .skip(pageNumber - 1)
+            .skip((pageNumber - 1)*pageSize)
             .limit(pageSize)
             .toArray()
 
