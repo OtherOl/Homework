aa6a9b5dc2f05a52cfa584961ad2e59ef2d51740
OtherOl <pilyak003@gmail.com>
2023-10-12 21:23:40 +0300

    CRUD User

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,12 +1,18 @@
 import {Response, Router, Request} from "express";
 import {usersService} from "../domain/users-service";
+import {bodyUserValidation} from "../middlewares/body-user-validation";
+import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
 
 export const authRouter = Router({})
 
-authRouter.post('/login', async (req: Request, res: Response) => {
+authRouter.post('/login',
+    bodyUserValidation.login, bodyUserValidation.password,
+    inputValidationMiddleware,
+    async (req: Request, res: Response) => {
     const login = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
 
     if(!login) {
         return res.sendStatus(401)
     }
+    return res.sendStatus(204)
 })
