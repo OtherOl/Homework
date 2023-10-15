ae7c26eff68261389ba117a425ef644112759e90
OtherOl <pilyak003@gmail.com>
2023-10-16 00:41:53 +0300

    JWT TOKEN

---
+++ src/middlewares/body-auth-validation.ts
@@ -0,0 +1,6 @@
+import {body} from "express-validator";
+
+export const bodyAuthValidation = {
+    loginOrEmail:body('loginOrEmail').isString().notEmpty().trim(),
+    password:body('password').isString().notEmpty().trim()
+}
