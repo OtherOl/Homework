4423bc6e95a3bc997a70e541619dc8b8ce289616
OtherOl <pilyak003@gmail.com>
2023-10-31 19:22:31 +0300

    RefreshToken

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -3,13 +3,13 @@ import {jwtService} from "../application/jwt-service";
 import {usersService} from "../domain/users-service";
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.headers.authorization) {
+    if (!req.headers.authorization || req.cookies['refreshToken']) {
         return res.sendStatus(401)
     }
 
     const token = req.headers.authorization.split(" ")[1]
-    const userId = await jwtService.getUserIdByToken(token)
-
+    const refToken = req.cookies['refreshToken'].split(" ")[1]
+    const userId = await jwtService.getUserIdByToken(token || refToken)
 
     if (userId) {
         req.user = await usersService.findUserById(userId)
