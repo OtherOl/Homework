47256d0b197264e6c0c172f452d360065edb38e8
OtherOl <pilyak003@gmail.com>
2023-11-12 17:06:34 +0300

    security/devices

--- src/routers/security-router.ts
+++ src/routers/security-router.ts
@@ -2,6 +2,7 @@ import {Router, Request, Response} from "express";
 import {jwtService} from "../application/jwt-service";
 import {devicesRepository} from "../repositories/devices-db-repositoty";
 import {tokensMiddleware} from "../middlewares/tokens-middleware";
+import {authRepository} from "../repositories/auth-db-repository";
 
 export const securityRouter = Router({})
 
@@ -24,13 +25,16 @@ securityRouter.delete('/devices',
 securityRouter.delete('/devices/:deviceId',
     tokensMiddleware,
     async (req: Request, res: Response) => {
+    const refreshToken = req.cookies.refreshToken
     const reqId = req.params.deviceId;
-    if(!reqId) return res.status(404).send({errorsMessage: []})
-    const verify = await jwtService.verifyToken(req.cookies.refreshToken);
+    if(!reqId) return res.sendStatus(404)
+    // if(!reqId) return res.status(404).send({errorsMessage: []});
+    const verify = await jwtService.verifyToken(refreshToken);
     const input = await devicesRepository.getSessionById(reqId);
 
     if(!input) return res.sendStatus(404)
     if (input.userId !== verify.userId) return res.sendStatus(403);
-    await devicesRepository.deleteSessionById(reqId)
+    await devicesRepository.deleteSessionById(reqId);
+    await authRepository.blackList(refreshToken)
     res.sendStatus(204)
 })
